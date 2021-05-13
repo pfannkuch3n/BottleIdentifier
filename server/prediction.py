9c8ee3e5e65387f0715991a4b2341caee29b46e7
pfannkuch3n <leon.kopka@rwth-aachen.de>
2021-05-13 18:57:22 +0700

    merged prediction_module with server folder

--- server/prediction.py
+++ server/prediction.py
@@ -1,9 +1,11 @@
 #!/bin/python3
 import sys
-import tensorflow
+import tensorflow as tf
 '''
     Get variable 
     weight = sys.argv[1];
 '''
 # print ('Argument List:', str(sys.argv[1]))
 print (str(sys.argv[0]))
+
+prediction_model = tf.keras.models.load_model('/
