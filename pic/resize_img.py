c1df5a817bdf9bd899def6573c35a4028c16879e
pfannkuch3n <leon.kopka@rwth-aachen.de>
2021-05-14 12:04:05 +0700

    resize

--- pic/resize_img.py
+++ pic/resize_img.py
@@ -5,7 +5,7 @@ import glob
 
 image = Image.open("/media/storage/pic/103APPLE/IMG_3333.JPG")
 print(image.size)
-resized_image = image.resize((int(image.size[0]/6),int(image.size[1]/6)))
+resized_image = image.resize((int(image.size[0]/8),int(image.size[1]/8)))
 
 print(resized_image.size)
 resized_image.save("/home/leon/BottleIdentifier/pic/classifier.jpg")
