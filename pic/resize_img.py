b8e62001c40ffb605f6a577e771f2b1c57d478dc
pfannkuch3n <leon.kopka@rwth-aachen.de>
2021-05-14 12:35:25 +0700

    add pictures

--- pic/resize_img.py
+++ pic/resize_img.py
@@ -3,9 +3,9 @@ import os
 import PIL
 import glob
 
-image = Image.open("/media/storage/pic/103APPLE/IMG_3337.JPG")
+image = Image.open("/media/storage/pic/103APPLE/IMG_3332.JPG")
 print(image.size)
 resized_image = image.resize((int(image.size[0]/16),int(image.size[1]/16)))
-
+resized_image= resized_image.transpose(Image.FLIP_TOP_BOTTOM)
 print(resized_image.size)
-resized_image.save("/home/leon/BottleIdentifier/pic/height.jpg")
+resized_image.save("/home/leon/BottleIdentifier/pic/weight.jpg")
