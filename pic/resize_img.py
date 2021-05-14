423bc3b962396ba62671be1d7f2ee83f1e63408a
pfannkuch3n <leon.kopka@rwth-aachen.de>
2021-05-14 11:56:15 +0700

    merge

---
+++ pic/resize_img.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import os
+import PIL
+import glob
+
+image = Image.open("/media/storage/pic/103APPLE/IMG_3333.JPG")
+print(image.size)
+resized_image = image.resize((int(image.size[0]/6),int(image.size[1]/6)))
+
+print(resized_image.size)
+resized_image.save("/home/leon/BottleIdentifier/pic/classifier.jpg")
